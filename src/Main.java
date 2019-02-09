57abb68716f54485dbd460389bd103b90466a2b1
jlc94087 <42546600+jlc94087@users.noreply.github.com>
2019-02-08 23:16:44 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        
     }
 
     public static void main(String[] args) {
