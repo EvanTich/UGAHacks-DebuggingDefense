9c131c29b63862b18a7497cf5f3af2c8bed565f0
jlc94087 <42546600+jlc94087@users.noreply.github.com>
2019-02-09 21:32:20 -0500

    merge fix

--- src/ugahacks/bug/defense/field/Bug.java
+++ src/ugahacks/bug/defense/field/Bug.java
@@ -12,12 +12,9 @@ public class Bug {
     private final Image BUG2 = new Image("ugahacks/UGAHacksAssets/Bug2.png");
     private final Image BUG3 = new Image("ugahacks/UGAHacksAssets/Bug3.png");
 
-<<<<<<< HEAD
-=======
     private final int WIDTH = 12;
     private final int HEIGHT = 12;
 
->>>>>>> 759337d77ffe66194fb2411e700ef27c46d19462
     Pos pos;
     int hp;
 
